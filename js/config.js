15fdb6cab085341524549cf928fc6f0a1f351228
KEVIN910919 <84594248+KEVIN910919@users.noreply.github.com>
2026-01-05 01:28:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -34,12 +34,6 @@ const CONFIG = {
       platform: "youtube",
       name: "Augustus Ling Ch. 奧古斯都‧白翎",
       avatar: "https://yt3.googleusercontent.com/lgg4GTnUNPmfgSMiicRX-3yg6tJyF7B7Fb6pDlkj8yTrJAZ4mXz6rKYv-48qAOKloZkIS4t_OSI=s160-c-k-c0x00ffffff-no-rj"
-    },
-    {
-      id: "UCgdwtyqBunlRb-i-7PnCssQ",
-      platform: "youtube",
-      name: "木棉花",
-      avatar: "https://yt3.googleusercontent.com/4osUNFW5VqcTvmGkShQtJ9JcrbpGUw9GX2291F9rW6fu9sx9q-VatUciXgqkNLiWh5u-b5OMMRo=s160-c-k-c0x00ffffff-no-rj"
     }
   ]
 };
