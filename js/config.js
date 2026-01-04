2c36350808af8149a337fa4d960d2d17d1397734
KEVIN910919 <84594248+KEVIN910919@users.noreply.github.com>
2026-01-04 19:48:23 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,20 +3,19 @@ const CONFIG = {
     {
       id: "mo_chan_neko",
       platform: "twitch",
+      name: "陌塵マコト",
+      avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/1b919743-51db-4e52-b7d2-9dae2c9e90d9-profile_image-70x70.jpeg",
       twitch: {
-        channel: "mo_chan_neko",
-        customProfile: true,
-        name: "陌塵マコト",
-        avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/1b919743-51db-4e52-b7d2-9dae2c9e90d9-profile_image-70x70.jpeg"
+        channel: "mo_chan_neko"
       }
     },
     {
       id: "AugustusLing",
       platform: "youtube",
+      name: "Augustus Ling Ch. 奧古斯都‧白翎",
+      avatar: "https://yt3.googleusercontent.com/lgg4GTnUNPmfgSMiicRX-3yg6tJyF7B7Fb6pDlkj8yTrJAZ4mXz6rKYv-48qAOKloZkIS4t_OSI=s160-c-k-c0x00ffffff-no-rj",
       youtube: {
-        channelId: "UCv0dlxt2yZf2U-n8kyA5hRQ",
-        name: "Augustus Ling Ch. 奧古斯都‧白翎",
-        avatar: "https://yt3.googleusercontent.com/lgg4GTnUNPmfgSMiicRX-3yg6tJyF7B7Fb6pDlkj8yTrJAZ4mXz6rKYv-48qAOKloZkIS4t_OSI=s160-c-k-c0x00ffffff-no-rj"
+        channelId: "UCv0dlxt2yZf2U-n8kyA5hRQ"
       }
     }
   ]
