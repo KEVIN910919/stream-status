729dd4fbe1360cde8f4785de4450c57bbdb72d81
KEVIN910919 <84594248+KEVIN910919@users.noreply.github.com>
2026-01-04 21:58:13 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,24 @@ const CONFIG = {
   
   channels: [
     {
+      id: "ilissia777",
+      platform: "twitch",
+      name: "伊莉西亞Ilissia",
+      avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/d4ec06ee-5aec-4271-a98f-bd2dbc652e00-profile_image-70x70.png",
+      twitch: {
+        channel: "ilissia777"
+      }
+    },
+    {
+      id: "jinzymiiii",
+      platform: "twitch",
+      name: "槿茲米",
+      avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/a8a06cbd-74af-4792-86b6-30bbaa73e4a3-profile_image-70x70.png",
+      twitch: {
+        channel: "jinzymiiii"
+      }
+    },
+    {
       id: "mo_chan_neko",
       platform: "twitch",
       name: "陌塵マコト",
@@ -16,15 +34,6 @@ const CONFIG = {
       platform: "youtube",
       name: "Augustus Ling Ch. 奧古斯都‧白翎",
       avatar: "https://yt3.googleusercontent.com/lgg4GTnUNPmfgSMiicRX-3yg6tJyF7B7Fb6pDlkj8yTrJAZ4mXz6rKYv-48qAOKloZkIS4t_OSI=s160-c-k-c0x00ffffff-no-rj"
-    },
-    {
-      id: "algae0619",
-      platform: "twitch",
-      name: "搞事的貓_藻藻",
-      avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/360ff93d-5677-4031-8824-0c5842cd0a2e-profile_image-70x70.png",
-      twitch: {
-        channel: "algae0619"
-      }
     }
   ]
 };
