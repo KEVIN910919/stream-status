ced2c6a5e10db774c489fdec88a3eb5327527ad1
KEVIN910919 <84594248+KEVIN910919@users.noreply.github.com>
2026-01-04 19:34:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,20 @@
 const CONFIG = {
-  twitch: {
-    channel: "mo_chan_neko"
-  }
+  channels: [
+    {
+      id: "mo_chan_neko",
+      platform: "twitch",
+      twitch: {
+        channel: "mo_chan_neko"
+      }
+    },
+    {
+      id: "b",
+      platform: "youtube",
+      youtube: {
+        channelId: "UCv0dlxt2yZf2U-n8kyA5hRQ",
+        name: "Augustus Ling Ch. 奧古斯都‧白翎",
+        avatar: "https://yt3.googleusercontent.com/lgg4GTnUNPmfgSMiicRX-3yg6tJyF7B7Fb6pDlkj8yTrJAZ4mXz6rKYv-48qAOKloZkIS4t_OSI=s160-c-k-c0x00ffffff-no-rj"
+      }
+    }
+  ]
 };
